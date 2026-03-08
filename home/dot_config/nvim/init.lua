46ca6af91378141487c4c5c88f052582867173a6
Adam Cooper <adam.cooper@gmail.com>
2026-03-07 18:25:56 -0800

    Some more updates

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,5 +1,26 @@
 vim.g.mapleader = " "
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep,  { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers,    { desc = "Buffers" })
+      vim.keymap.set("n", "<leader>fk", builtin.keymaps,   { desc = "Keymaps" })
+    end,
+  },
+})
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
